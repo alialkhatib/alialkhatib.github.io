f814cb9a7dc628b4e086998bd5d88a848e113020
Ali Alkhatib <hi@al2.in>
2023-06-27 09:14:31 -0400

    tech conference bingo

---
+++ bingo/conference_bullshit.js
@@ -0,0 +1,46 @@
+bullshit=[
+// "marketplace of ideas",
+// "all ideologies are valid",
+"there IS some evidence for phrenology",
+"we talked to a person of color",
+"national security blah blah blah",
+"if we don't do it, china will. then what??",
+"it's legal, so it's fine",
+// "Manhattan project",
+"this is just math; ethics isn't our problem",
+"Mirror on society",
+"generative AI or LLMs (no further specificity)",
+"It was performance art",
+"We were going to ask an ethicist later",
+"Our IRB (/some legal council) said it was fine",
+"anything \"innovation\"", // https://twitter.com/hypervisible/status/1288214504371888129
+"\"values\", without specifying further", // https://twitter.com/hypervisible/status/1288214504371888129
+"\"AI\", without specifying further", // https://twitter.com/hypervisible/status/1288214504371888129
+"self-regulation", // https://twitter.com/jeannieccrowley/status/1288215634279567361
+"if you don't code, you can't possibly understand", // inspired by https://twitter.com/EylerWerve/status/1288219971672256512 and https://twitter.com/AndrewNoymer/status/1288216098718879744
+"everything is biased, so *nothing* matters",
+"we're constantly iterating (on you)", // inspired by https://twitter.com/seefloor/status/1288217939779305472
+// "we don't sell data", // inspired by https://twitter.com/hypervisible/status/1288217132585693184
+// "consumers have a choice", // https://twitter.com/hypervisible/status/1288217132585693184
+// "lots of jobs tied up in this", // https://twitter.com/hypervisible/status/1288217132585693184
+// "we refuse to profit from hate", // https://twitter.com/hypervisible/status/1288217132585693184
+"technology (/data) is neutral", // inspired by https://twitter.com/EylerWerve/status/1288219971672256512
+"It's simply a matter of obtaining better training data", // from https://twitter.com/stuartmarks/status/1288309243573985280
+// "this isn't a problem if you have nothing to hide", // https://twitter.com/stuartmarks/status/1288309243573985280
+"the remedy to bad tech is more good tech", // from https://twitter.com/steveglista/status/1288220035287310336
+"first amendment", // one of many borroed from @StenderWorld's bingo card
+"terrorism"
+"CDA 230", // @StenderWorld
+// "Arab Spring", // @StenderWorld
+// "we're a platform, not a media company", // @StenderWorld
+// "Encryption", // @StenderWorld
+"what about boobs?", // inspired but not strictly @StenderWorld's card
+// "surveillance CEO doesn't want to be recorded",
+"\"It's human nature\"",
+"think of the children",
+"\"this is more of a comment than a question...\"",
+"acronym soup!",
+"TikTok (very unsubtle Sinophobia)",
+"unsubtle self-cite",
+"\"The\" Algorithm"
+]
